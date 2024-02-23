1dcd5f0c09be9e116a1ba22e8ceb946002349f9c
gratachu <akitatsu95.climbing@gmail.com>
2024-02-23 17:14:33 +0900

    Add: theme swich function

--- app/components/ThemeSwitch.tsx
+++ app/components/ThemeSwitch.tsx
@@ -1,6 +1,24 @@
+"use client"
+
+import {useTheme} from "next-themes";
+import {useEffect, useState} from "react";
+
 const ThemeSwitch = () => {
+  const [ mounted, setMounted ] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted) {
+    return null;
+  }
+
   return (
-    <button>Theme Switch</button>
+    <button
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+    >
+      {theme === "dark" ? "Light" : "Dark"}
+    </button>
   );
 }
 
